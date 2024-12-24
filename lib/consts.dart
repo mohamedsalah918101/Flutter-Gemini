008a194aad587abe73bc6089eb693204446d3866
Mohamed Salah <89584435+mohamedsalah918101@users.noreply.github.com>
2024-12-24 21:30:44 +0200

    Flutter app with Gemini integration

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,1 +1,3 @@
+const apiKey = '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';
 
+const String GEMINI_API_KEY = "AIzaSyBZUurUx_zsj1UM6mMUsSF2qQ-As_H8s-g";
